64062536a7bbf37760075b3cb17f6f5d72625f7d
garrett cullen <info@garrettcullen.com>
2016-04-15 14:27:06 -0700

    push

--- wp-content/themes/adpages/page-adtemplate.php
+++ wp-content/themes/adpages/page-adtemplate.php
@@ -12,22 +12,60 @@
  * @since Twenty Ten 1.0
  */
 
-get_header(); ?>
+ ?>
+ 
+ <?php
+/**
+ * Header template for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	global $page, $paged;
 
-		<div id="container">
-			<div id="content" role="main">
+	wp_title( '|', true, 'right' );
 
-			<?php
-			/*
-			 * Run the loop to output the page.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-page.php and that will be used instead.
-			 */
-			get_template_part( 'loop', 'page' );
-			?>
+	// Add the blog name.
+	bloginfo( 'name' );
 
-			</div><!-- #content -->
-		</div><!-- #container -->
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		echo " | $site_description";
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+	// Add a page number if necessary:
+	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() )
+		echo esc_html( ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) ) );
+
+	?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+
+	
+<?php // get_template_part( 'loop', 'page' ); ?> 
+
+		
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
